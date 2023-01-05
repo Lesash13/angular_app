f8f3c8880425cacd6acc31f231c773cb9b4a74f3
Lesash13 <18631883+lesash13@users.noreply.github.com>
2023-01-05 19:35:58 +0300

    edit post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {YourFeedModule} from './yourFeed/yourFeed.module'
 import {TagFeedModule} from 'src/app/tagFeed/tagFeed.module'
 import {ArticleModule} from './article/article.module'
 import {CreateArticleModule} from './createArticle/createArticle.module'
+import {EditArticleModule} from './editArticle/editArticle.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -38,6 +39,7 @@ import {CreateArticleModule} from './createArticle/createArticle.module'
     YourFeedModule,
     TagFeedModule,
     CreateArticleModule,
+    EditArticleModule,
     ArticleModule,
   ],
   providers: [
